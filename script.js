53d9b8e28ca8b8b00b3d3f34dfdd0c9e20119137
Paula Peña <73245170+paupenfer1@users.noreply.github.com>
2022-11-20 13:51:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = "Paula Peña Fernández"
+let age = 20
+let agex7 = 7*age
+let friends = ["Lorena", "Lucía", "Aitor"]
+let me = {name, age, isStudent:true}
+let arrays_friends = [{name:"Lorena", age:20, isStudent:true}, {name:"Lucía", age:19, isStudent:true},{name:"Aitor", age:21, isStudent:true}]
+
+console.log(name, age, agex7, friends, me, arrays_friends);
